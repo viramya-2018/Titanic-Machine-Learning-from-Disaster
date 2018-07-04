628cacf8a390793fa25c4044f163608828886b14
viramya-2018 <37098731+viramya-2018@users.noreply.github.com>
2018-07-04 17:47:37 +0530

    Submssion v2

--- Scripts/model.py
+++ Scripts/model.py
@@ -4,6 +4,9 @@ import sys
 import os
 from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.neural_network import MLPClassifier
 from sklearn.metrics import accuracy_score
 
 
@@ -16,22 +19,26 @@ testData = testFile.values
 X = trainData[:,2:]
 y = trainData[:,1]
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.15, random_state=16)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.50, random_state=16)
 
 print len(X_train), len(X_test), len(y_train), len(y_test)
 
-clf = SVC()
+# clf = SVC(C=0.6, kernel='rbf')
+# clf = DecisionTreeClassifier(criterion = 'entropy', random_state = 3)
+# clf = GaussianNB()
+
+clf = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(20, 4), random_state=1, learning_rate_init=0.01, activation='tanh')
 clf.fit(X_train, y_train)
 
+print accuracy_score(y_test, np.array(clf.predict(X_test)))
 
 X_testData = testData[:,1:]
-print X_testData
 
-predictLabel = np.array(clf.predict(X_testData))
+predictLabel_submit = np.array(clf.predict(X_testData))
 
 directory = "../result/"
 if not os.path.exists(directory):
     os.makedirs(directory)
 
-res = pd.DataFrame(data=predictLabel, columns=['Survived'])
+res = pd.DataFrame(data=predictLabel_submit, columns=['Survived'])
 res.to_csv(directory + "submit.csv")
--- Scripts/preprocessing.py
+++ Scripts/preprocessing.py
@@ -3,11 +3,14 @@ import pandas as pd
 import math
 import sys
 import os
+from matplotlib import pyplot as plt
 
 fileName = sys.argv[1]
 fileName = fileName + ".csv"
 data = pd.read_csv("../data/" + fileName)
 data = pd.DataFrame(data)
+
+# print data.describe()
 
 # mapping male to 1 and female to 0
 mappingSex = {'male': 1, 'female': 0}
@@ -22,16 +25,31 @@ meanAge_female = data['Age'].groupby(data['Sex']).describe()[0]['mean']
 meanAge_male = data['Age'].groupby(data['Sex']).describe()[1]['mean']
 
 # replacing NaN with mean ages of males and females respectively
-# currently replacing with only the mean!
-data['Age'] = data['Age'].fillna(value=math.ceil(data['Age'].mean()))
+arr = data.values
+# print arr
+# print data.describe()
+if(sys.argv[1] == 'train'):
+    for i in arr:
+        if i[4] == 0 and not (i[5] == i[5]):
+            i[5] = meanAge_female
+        elif i[4] == 1 and not (i[5] == i[5]):
+            i[5] = meanAge_male
+if(sys.argv[1] == 'test'):
+    for i in arr:
+        if i[3] == 0 and not (i[4] == i[4]):
+            i[4] = meanAge_female
+        elif i[3] == 1 and not (i[4] == i[4]):
+            i[4] = meanAge_male
+
+data.iloc[:,:] = arr
 
 # replacing NaN with 0 for embarked as it the most common source
 data['Embarked'] = data['Embarked'].fillna(value=0.0)
 
 if(sys.argv[1] == 'train'):
-    data = data.loc[:, ['Survived', 'Pclass', 'Sex', 'Age']]
+    data = data.loc[:, ['Survived', 'Pclass', 'Sex', 'Age', 'SibSp', 'Parch']]
 if(sys.argv[1] == 'test'):
-    data = data.loc[:, ['Pclass', 'Sex', 'Age']]
+    data = data.loc[:, ['Pclass', 'Sex', 'Age', 'SibSp', 'Parch']]
 
 directory = "../data/processed_data/"
 
